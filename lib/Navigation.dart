4020dcbeb1ce672d36393b7677853ab911a9ca4b
salehanoor644 <salehanoor896@gmail.com>
2020-12-07 04:16:41 -0800

    Commit 1 Splash_Screen.dart

---
+++ lib/Navigation.dart
@@ -0,0 +1,135 @@
+
+import 'package:second_sestional/Hard_Dice.dart';
+import 'package:second_sestional/Simple_Dice.dart';
+import 'package:flutter/material.dart';
+
+
+class  HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Dice App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+        Row(
+        children: [
+        Expanded(
+
+        child: Padding(
+            padding: EdgeInsets.all(15.0),
+        child: FlatButton(
+          textColor: Colors.white,
+          color: Colors.green,
+          child: Text(
+            'Simple',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+          onPressed: simpleDice,
+        ),
+      ),
+    ),
+    Expanded(
+
+    child: Padding(
+    padding: EdgeInsets.all(15.0),
+    child: FlatButton(
+
+    textColor: Colors.white,
+    color: Colors.black,
+    child: Text(
+    'Hard',
+    style: TextStyle(
+    color: Colors.white,
+    fontSize: 20.0,
+    ),
+    ),
+          onPressed: hardDice,
+          ),
+    ),
+    ),
+        ],
+          ),]
+
+      ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text('Saleha Noor'),
+                accountEmail: Text('Salehanoor896@gmail.com'),
+                currentAccountPicture: GestureDetector(
+                  onTap: ()=> _showSecondPage(context),
+                  child: CircleAvatar(
+                      backgroundImage: AssetImage('images/image.png'),
+                  ),
+                )
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Account'),
+              subtitle: Text('Personal'),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone_android),
+              title: Text('Phone'),
+              subtitle: Text('03072251415'),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('Email'),
+              subtitle: Text('Salehanoor896@gmail.com'),
+              trailing: Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+void simpleDice() {
+  setState(() {
+    Navigator.push(context, new MaterialPageRoute(builder: (context) => new SimpleDice()));
+  });
+}
+
+void hardDice() {
+  setState(() {
+    Navigator.push(context, new MaterialPageRoute(builder: (context) => new HardDice()));
+  });
+}
+  void _showSecondPage(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => Scaffold(
+          body: Center(
+            child: Hero(
+              tag: 'my-hero-animation-tag',
+              child: Image.asset('images/image.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Splash_Screen.dart
@@ -0,0 +1,48 @@
+import 'package:second_sestional/main.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+import 'main.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Future<Widget> loadFromFuture() async {
+
+    // <fetch data from server. ex. login>
+
+    return Future.value(new Home());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+
+        seconds: 8,
+        navigateAfterSeconds: new Home(),
+
+
+        title:Text('Saleha Noor\n''FA17-BSE-073',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,),
+        ),
+
+
+        image: new Image.asset('image.png'),
+
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.red
+    );
+  }
+}
